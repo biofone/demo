b1a6697ac485edde3cccac1b86495cce5f37d8fd
biofone <lichao_sean@163.com>
2020-01-10 20:19:04 +0800

    add fileUpload-demo

---
+++ file-up-down-demo/src/test/java/com/chao/fileupdowndemo/FileUpDownDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chao.fileupdowndemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUpDownDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
